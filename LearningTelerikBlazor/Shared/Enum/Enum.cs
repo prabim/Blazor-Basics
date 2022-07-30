36519eedb058347b8144fe5e53ab0d3485674a34
Prabeam <prabeam@gmail.com>
2022-07-30 23:21:47 +0545

    Adding the project files

---
+++ LearningTelerikBlazor/Shared/Enum/Enum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LearningTelerikBlazor.Shared.Enum
+{
+    public  class Enum
+    {
+        public enum Sizes
+        {
+            [Display(Name = "Select a size")]
+            NoneSelected,
+            Small, 
+            Medium,
+            Large,
+            [Display(Name = "Very Special")]
+            Special
+        }
+    }
+}
---
+++ LearningTelerikBlazor/Shared/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static LearningTelerikBlazor.Shared.Enum.Enum;
+
+namespace LearningTelerikBlazor.Shared
+{
+    public class Order
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        public string Alias { get; }
+        [Range(1,4)]
+        public Sizes Size { get; set; } = Sizes.Small;
+        public bool Decaf { get; set; }
+        public bool Iced { get; set; }
+        [MaxLength(length:1000)]
+        public string Comment { get; set; }
+        public double Tip { get; set; }
+
+    }
+}
